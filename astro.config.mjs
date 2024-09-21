da532343ea966b2201695060888e5ae74b02549d
Juan Carlos <Juank199819@gmail.com>
2024-09-21 19:02:19 +0200

    Change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://xCarlos19.github.io',
+  base: 'studio-film',
 });
