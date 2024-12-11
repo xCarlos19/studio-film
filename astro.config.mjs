3ea09ebbc9dab66d9610a42d6bfb0db34f423655
Juan Carlos <Juank199819@gmail.com>
2024-12-11 20:18:24 +0100

    Migrate to Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,19 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  experimental:{env: {
+  env: {
     schema:{
       EMAIL_FROM: envField.string({context:'client',access:'public'}),
       EMAIL_DEST: envField.string({context:'client',access:'public'}),
       RESEND_API_KEY: envField.string({context:'client',access:'public'}),
-    }
+    
     // Add environment variables here...
   }},
   site: 'https://xCarlos19.github.io',
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -9,7 +10,22 @@ export default {
 				sans: ['Courier Prime', ...defaultTheme.fontFamily.sans],
 				text:['Oswald Variable', ...defaultTheme.fontFamily.sans],
 			  },
-		}
+		},
+		animation: {
+			"background-shine": "background-shine 2s linear infinite"
+			  },
+			  keyframes: {
+			"background-shine": {
+			  "from": {
+				"backgroundPosition": "0 0"
+			  },
+			  "to": {
+				"backgroundPosition": "-200% 0"
+			  }
+			}
+		  }
 	},
 	plugins: [],
+	
+  
 }
